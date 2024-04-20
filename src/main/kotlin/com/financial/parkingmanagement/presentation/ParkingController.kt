3794a192338eb2a1709e75c2c640986160d2044b
marcosoliveira <nonato.729@gmail.com>
2024-04-20 14:12:28 -0400

    rename parking for vacancy

--- src/main/kotlin/com/financial/parkingmanagement/presentation/ParkingController.kt
+++ src/main/kotlin/com/financial/parkingmanagement/presentation/ParkingController.kt
@@ -1,14 +1,14 @@
 package com.financial.parkingmanagement.presentation
 
-import com.financial.parkingmanagement.domain.ParkingUseCase
 import com.financial.parkingmanagement.domain.Vacancy
+import com.financial.parkingmanagement.domain.VacancyUseCase
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class ParkingController(
-    private val useCase: ParkingUseCase
+    private val useCase: VacancyUseCase
 ) {
 
     @GetMapping("/allVacancy")
