c16868ee09b2d21a4e523d6740cf44bf8d46ad11
marcosoliveira <nonato.729@gmail.com>
2024-04-21 16:06:14 -0400

    UPDATE QUERY ALL REGISTERS

--- src/main/kotlin/com/financial/parkingmanagement/data/RegisterRepository.kt
+++ src/main/kotlin/com/financial/parkingmanagement/data/RegisterRepository.kt
@@ -5,16 +5,20 @@ import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
-interface RegisterRepository: JpaRepository<RegisterModel, Long> {
+interface RegisterRepository : JpaRepository<RegisterModel, Long> {
 
-    @Query("SELECT *\n" +
-            "FROM register\n" +
-            "WHERE TRIM(date_entry) IS NOT NULL AND TRIM(date_entry) <> '';", nativeQuery = true)
+    @Query(
+        "SELECT * FROM register WHERE TRIM(date_entry) IS NOT NULL AND TRIM(date_entry) <> ''" +
+                " AND register.date_exit IS NULL OR length(register.date_exit) = 0;",
+        nativeQuery = true
+    )
     fun findAllByDateEntry(): MutableList<RegisterModel>
 
-    @Query("SELECT *\n" +
-            "FROM register\n" +
-            "WHERE TRIM(date_exit) IS NOT NULL AND TRIM(date_exit) <> '';", nativeQuery = true)
+    @Query(
+        "SELECT *\n" +
+                "FROM register\n" +
+                "WHERE TRIM(date_exit) IS NOT NULL AND TRIM(date_exit) <> '';", nativeQuery = true
+    )
     fun findAllByDateExit(): MutableList<RegisterModel>
 
     override fun findAll(): MutableList<RegisterModel>
